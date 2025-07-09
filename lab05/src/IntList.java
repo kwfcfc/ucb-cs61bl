fe1f25f0ca045a11ad965aaa44dd1b0dfd5efc96
kwfcfc <73300175+kwfcfc@users.noreply.github.com>
2025-07-09 16:56:28 +0000

    lab05: finish get, toString and equals

--- lab05/src/IntList.java
+++ lab05/src/IntList.java
@@ -1,5 +1,6 @@
-/** A data structure to represent a Linked List of Integers.
- * Each IntList represents one node in the overall Linked List.
+/**
+ * A data structure to represent a Linked List of Integers. Each IntList represents one node in the
+ * overall Linked List.
  */
 
 public class IntList {
@@ -20,9 +21,10 @@ public class IntList {
         this(item, null);
     }
 
-    /** Returns an IntList consisting of the elements in ITEMS.
-     * IntList L = IntList.list(1, 2, 3);
-     * System.out.println(L.toString()) // Prints 1 2 3 */
+    /**
+     * Returns an IntList consisting of the elements in ITEMS. IntList L = IntList.list(1, 2, 3);
+     * System.out.println(L.toString()) // Prints 1 2 3
+     */
     public static IntList of(int... items) {
         /** Check for cases when we have no element given. */
         if (items.length == 0) {
@@ -40,38 +42,54 @@ public class IntList {
     }
 
     /**
-     * Returns [position]th item in this list. Throws IllegalArgumentException
-     * if index out of bounds.
+     * Returns [position]th item in this list. Throws IllegalArgumentException if index out of
+     * bounds.
      *
      * @param position, the position of element.
      * @return The element at [position]
      */
     public int get(int position) {
-        // TODO: YOUR CODE HERE
-        return -1;
+        if (position < 0) {
+            throw new IllegalArgumentException("position should be no less than zero.");
+        }
+        int value = this.item;
+        IntList iterator = this.next;
+        while (position > 0) {
+            if (iterator == null) {
+                throw new IllegalArgumentException("index out of bound");
+            }
+            value = iterator.item;
+            iterator = iterator.next;
+            position--;
+        }
+        return value;
     }
 
     /**
-     * Returns the string representation of the list. For the list (1, 2, 3),
-     * returns "1 2 3".
+     * Returns the string representation of the list. For the list (1, 2, 3), returns "1 2 3".
      *
      * @return The String representation of the list.
      */
     public String toString() {
-        // TODO: YOUR CODE HERE
-        return null;
+        IntList iterator = this.next;
+        StringBuilder builder = new StringBuilder();
+        builder.append(this.item);
+        while (iterator != null) {
+            builder.append(" ");
+            builder.append(iterator.item);
+            iterator = iterator.next;
+        }
+        return builder.toString();
     }
 
     /**
      * Returns whether this and the given list or object are equal.
      *
-     * NOTE: A full implementation of equals requires checking if the
-     * object passed in is of the correct type, as the parameter is of
-     * type Object. This also requires we convert the Object to an
-     * IntList, if that is legal. The operation we use to do this is called
-     * casting, and it is done by specifying the desired type in
-     * parentheses. An example of this is `IntList otherLst = (IntList) obj;`
-     * We recommend reviewing the `instanceOf` keyword mentioned in the spec.
+     * NOTE: A full implementation of equals requires checking if the object passed in is of the
+     * correct type, as the parameter is of type Object. This also requires we convert the Object to
+     * an IntList, if that is legal. The operation we use to do this is called casting, and it is
+     * done by specifying the desired type in parentheses. An example of this is `IntList otherLst =
+     * (IntList) obj;` We recommend reviewing the `instanceOf` keyword mentioned in the spec.
      *
      * @param obj, another list (object)
      * @return Whether the two lists are equal.
@@ -81,8 +99,10 @@ public class IntList {
             return false;
         }
         if (obj instanceof IntList otherList) {
-            // TODO: your code here
-
+            boolean itemSame = this.item == otherList.item;
+            boolean bothNullOrEqualNext =
+                    this.next == null ? otherList.next == null : this.next.equals(otherList.next);
+            return itemSame && bothNullOrEqualNext;
         }
         return false;
     }
@@ -149,7 +169,8 @@ public class IntList {
         return res;
     }
 
-    /** Returns a list equal to L with all elements squared. Non-destructive.
+    /**
+     * Returns a list equal to L with all elements squared. Non-destructive.
      *
      * @param L list to non-destructively square.
      * @return the squared list.
@@ -162,8 +183,8 @@ public class IntList {
     }
 
     /**
-     * Returns a new IntList consisting of A followed by B,
-     * non-destructively (no modifications to A).
+     * Returns a new IntList consisting of A followed by B, non-destructively (no modifications to
+     * A).
      *
      * @param A list to be on the front of the new list.
      * @param B list to be on the back of the new list.
@@ -175,8 +196,7 @@ public class IntList {
     }
 
     /**
-     * Returns a new IntList consisting of A followed by B,
-     * destructively (make modifications to A).
+     * Returns a new IntList consisting of A followed by B, destructively (make modifications to A).
      *
      * @param A list to be on the front of the new list.
      * @param B list to be on the back of the new list.
