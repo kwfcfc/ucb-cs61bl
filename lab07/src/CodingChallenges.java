ce28e9413d6ae2a72fcf63bcbe8553ce461a2743
kwfcfc <73300175+kwfcfc@users.noreply.github.com>
2025-07-10 00:42:20 +0000

    lab07: finish CodingChallenges

--- lab07/src/CodingChallenges.java
+++ lab07/src/CodingChallenges.java
@@ -4,6 +4,7 @@ import java.util.Map;
 import java.util.Set;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class CodingChallenges {
 
@@ -12,7 +13,16 @@ public class CodingChallenges {
      * values from 0 to N except for one missing number.
      */
     public static int missingNumber(int[] values) {
-        // TODO
+        boolean found[] = new boolean[values.length + 1];
+        Arrays.fill(found, true);
+        for (int num: values) {
+            found[num] = false;
+        }
+        for (int i = 0; i < found.length; i++) {
+            if (found[i]) {
+                return i;
+            }
+        }
         return -1;
     }
 
@@ -21,7 +31,30 @@ public class CodingChallenges {
      * permutation of s2 if it has the same number of each character as s2.
      */
     public static boolean isPermutation(String s1, String s2) {
-        // TODO
-        return false;
+        if (s1 == null ^ s2 == null) {
+            return false;
+        }
+        if (s1.length() != s2.length()) {
+            return false;
+        }
+        HashMap<Character, Integer> count = new HashMap<>();
+        for (char c: s1.toCharArray()) {
+            if (count.get(c) == null) {
+                count.put(c, 1);
+            } else {
+                count.put(c, count.get(c) + 1);
+            }
+        }
+        // now use the count and the s2 char array.
+        for (char c: s2.toCharArray()) {
+            if (count.get(c) == null) {
+                return false;
+            } else if (count.get(c) == 1) {
+                count.remove(c);
+            } else {
+                count.put(c, count.get(c) - 1);
+            }
+        }
+        return count.isEmpty();
     }
 }
--- lab07/test/CodingChallengesTest.java
+++ lab07/test/CodingChallengesTest.java
@@ -1,16 +1,29 @@
 import org.junit.Test;
-
+import static com.google.common.truth.Truth.assertThat;
 import static com.google.common.truth.Truth.assertWithMessage;
 
 public class CodingChallengesTest {
 
     @Test
     public void testMissingNumber() {
-	// TODO
+        int values[] = {3, 5, 1, 4, 0};
+        assertThat(CodingChallenges.missingNumber(values)).isEqualTo(2);
     }
 
     @Test
     public void testIsPermutation() {
-	// TODO
+        String s1 = "barcelona";
+        String s2 = "ecornlbaa";
+        String moreString = "barcelonasss";
+        String lessString = "brclon";
+        String emptyString = "";
+        String nullString = null;
+
+        assertThat(CodingChallenges.isPermutation(s1, s2)).isTrue();
+        assertThat(CodingChallenges.isPermutation(s2, moreString)).isFalse();
+        assertThat(CodingChallenges.isPermutation(lessString, s1)).isFalse();
+        assertThat(CodingChallenges.isPermutation(emptyString, s1)).isFalse();
+        assertThat(CodingChallenges.isPermutation(nullString, s1)).isFalse();
     }
 }
+
