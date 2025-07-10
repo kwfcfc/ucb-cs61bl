1fc669a05e3d1c25582e6ebe26d8db9049376bc3
kwfcfc <73300175+kwfcfc@users.noreply.github.com>
2025-07-10 01:02:54 +0000

    lab07: finish ListSet.java.
    1. to-do more unit tests

--- lab07/src/ListSet.java
+++ lab07/src/ListSet.java
@@ -15,23 +15,22 @@ public class ListSet implements SimpleSet {
     /** Adds k to the set. */
     @Override
     public void add(int k) {
-        // TODO: Implement this method.
+        if (!elems.contains(k)) {
+            elems.add(k);
+        }
     }
 
     /** Removes k from the set. */
     @Override
     public void remove(int k) {
         Integer toRemove = k;
-        // TODO - use the above variable with an appropriate List method.
-        // The reason is beyond the scope of this lab, but involves
-        // method resolution.
+        elems.remove(toRemove);
     }
 
     /** Return true if k is in this set, false otherwise. */
     @Override
     public boolean contains(int k) {
-        // TODO: Implement this method.
-        return false;
+        return elems.contains(k);
     }
 
     /** Return true if this set is empty, false otherwise. */
@@ -43,14 +42,20 @@ public class ListSet implements SimpleSet {
     /** Returns the number of items in the set. */
     @Override
     public int size() {
-        // TODO: Implement this method.
-        return 0;
+        return elems.size();
     }
 
     /** Returns an array containing all of the elements in this collection. */
     @Override
     public int[] toIntArray() {
-        // TODO - use a for loop!
-        return null;
+        int result[] = new int[this.size()];
+
+        int i = 0;
+        for (int elem: elems) {
+            result[i] = elem;
+            i++;
+        }
+
+        return result;
     }
 }
