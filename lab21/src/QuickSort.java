cf8d992f80342ad9a9f42373feea2320adac36c7
kwfcfc <73300175+kwfcfc@users.noreply.github.com>
2025-08-10 23:24:40 +0000

    lab21: finish QuickSort

--- lab21/src/QuickSort.java
+++ lab21/src/QuickSort.java
@@ -21,8 +21,20 @@ public class QuickSort {
      * Helper method for sort: runs quicksort algorithm on array from [start:end)
      */
     private static void quickSort(int[] arr, int start, int end) {
-        // TODO: Implement quicksort
-        
+        if (end <= start + 1) return;
+
+        int[] slice = partition(arr, start, end);
+        quickSort(arr, start, slice[0]);
+        quickSort(arr, slice[1], end);
+    }
+
+    // set the start of the arr as the pivot from [start:end)
+    private static void setPivot(int[] arr, int start, int end) {
+        if (end - start <= 2) return;
+        int middle = start + (end - start) / 2;
+        int a = arr[start], b = arr[middle], c = arr[end - 1];
+        if ((a <= b && b <= c) || (c <= b && b <= a)) swap(arr, start, middle);
+        if ((a <= c && c <= b) || (b <= c && c <= a)) swap(arr, start, end - 1);
     }
 
     /**
@@ -39,7 +51,35 @@ public class QuickSort {
      * Most of the code for quicksort is in this function
      */
     private static int[] partition(int[] arr, int start, int end) {
-        // TODO: Implement partition
-        return null;
+        if (end <= start + 1) return new int[]{start, end};
+        setPivot(arr, start, end);
+        int pivot = arr[start];
+        int left = start + 1;
+        int right = end - 1;
+        while (left <= right) {
+            for (; arr[left] <= pivot && left <= right; ++left);
+            for (; arr[right] > pivot && left <= right; --right);
+
+            if (left <= right) {
+                swap(arr, left, right);
+            }
+        }
+
+        swap(arr, right, start);
+        return new int[]{right, left};
     }
+
+    /**
+     * @param arr
+     * @param i
+     * @param j
+     *
+     * Swap the elements at indices i and j in the array arr.
+     * A helper method you can use in your implementation of sort.
+     */
+    private static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i]  = arr[j];
+        arr[j]  = temp;
+    }    
 }   
